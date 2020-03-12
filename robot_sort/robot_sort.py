dd9e179049893ce69e9509c51692d09174a84aee
Yilun Weng <wengellen@gmail.com>
2020-03-11 18:27:36 -0700

    Finish robot_sort.py using bubble sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -101,23 +101,26 @@ class SortingRobot:
         # compare item at position with the one he is holding
         # if it's less than the one in position, swap it
         # else compare with next slot in the list
-        self.swap_item()
-        print(self._item)
-        print( self.can_move_right())
-        while self.can_move_right():
-            self.move_right()
-            print(f'move right - item {self._item}')
-            if self.compare_item() == -1:
+        while not self.light_is_on():
+            self.set_light_on()
+
+            while self.can_move_right():
                 self.swap_item()
+                self.move_right()
+                if self.compare_item() and self.compare_item() > 0:
+                    self.swap_item()
+                    self.set_light_off()
 
-
-
+            while self.can_move_left():
+                self.move_left()
+                self.swap_item()
+# bubble sort or selection sort
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58 ]
+    l = [60, 30, 100,50, 5, 15]
     #l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
